eb1f8669729de0c43233920deb7c11919f118d01
ex00o <40499945+ex00o@users.noreply.github.com>
2022-07-05 01:53:38 +0200

    Main page update

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+var db;
+
 initSqlJs().then(function () {
   const sqlPromise = initSqlJs();
 
   const dataPromise = fetch("db.sqlite").then((res) => res.arrayBuffer());
   Promise.all([sqlPromise, dataPromise]).then(([SQL, buf]) => {
-    const db = new SQL.Database(new Uint8Array(buf));
-    const res = db.exec("SELECT * FROM Dictionary");
-    console.log(res);
+  db = new SQL.Database(new Uint8Array(buf));
+    
   });
 
   // // Prepare a statement
@@ -21,22 +22,21 @@ initSqlJs().then(function () {
 });
 
 let section_id = 0;
-
 function scrollDown() {
-  const sections = ["section-start", "section-about"];
+  const sections = ["#section-start", "#section-about"];
 
   if (section_id < sections.length - 1) section_id++;
 
   setTimeout(() => {
     for (let i = 0; i < sections.length; i++) {
       if (i != section_id)
-        document.getElementById(sections[i]).style.display = "none";
+        $(sections[i]).css("display", "none")
     }
   }, 440);
 
   console.log(section_id, sections[section_id]);
-  document.getElementsByTagName("html")[0].style.scrollBehavior = "smooth";
+  $("html")[0].style.scrollBehavior = "smooth";
   //document.body.style.overflowY = "visible";
-  document.getElementById(sections[section_id]).style.display = "inline-block";
-  window.location.href = "#" + sections[section_id];
+  $(sections[section_id]).css("display", "block");
+  window.location.href = sections[section_id];
 }
